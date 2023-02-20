19e8792fa9b307fc4fce0387d63bac2ba17ea76c
GoldenVikingSunset <Renni@GoldenVikingSunset.onmicrosoft.com>
2023-02-20 20:37:47 +0000

    Added User name and amount of guesses taken

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,17 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
+    // Declare and initialize a variable for guesses
+    let mut guesses = 0;
+
+    // Ask for and store the user name
+    println!("Please enter your name.");
+    let mut name = String::new();
+    io::stdin()
+        .read_line(&mut name)
+        .expect("Failed to read line");
+    let name = name.trim();
+
     loop {
         println!("Please input your guess.");
 
@@ -21,13 +32,17 @@ fn main() {
             Err(_) => continue,
         };
 
+        // Increment guesses by one
+        guesses += 1;
+
         println!("You guessed: {guess}");
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
-                println!("You win!");
+                // Print guesses and name
+                println!("You win, {name}! You took {guesses} guesses.");
                 break;
             }
         }
