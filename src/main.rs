efd0f1e954c77df2d85ced3b25313be0c69be854
GoldenVikingSunset <Renni@GoldenVikingSunset.onmicrosoft.com>
2023-02-22 01:09:45 +0000

    Added pont system and scoreboard

--- src/main.rs
+++ src/main.rs
@@ -1,101 +1,89 @@
 use std::io;
 use std::cmp::Ordering;
 use rand::Rng;
+use std::collections::HashMap;
 
 fn main() {
-    println!("Welcome to the guessing game!");
+    println!("Guess the number!");
+
+    // Ask for the difficulty level
+    let mut level = String::new();
+    println!("Please choose a difficulty level: easy (1-75), normal (1-150), hard (1-1000)");
+    io::stdin()
+        .read_line(&mut level)
+        .expect("Failed to read line");
+
+    let max = match level.trim() {
+        "easy" => 75,
+        "normal" => 150,
+        "hard" => 1000,
+        _ => {
+            println!("Invalid input. Using default value of normal (1-150).");
+            150
+        }
+    };
+
+    let secret_number = rand::thread_rng().gen_range(1..=max);
+
+    // Declare and initialize a variable for guesses
+    let mut guesses = 0;
+
+    // Ask for and store the user name
+    println!("Please enter your name.");
+    let mut name = String::new();
+    io::stdin()
+        .read_line(&mut name)
+        .expect("Failed to read line");
+    let name = name.trim();
+
+    // Declare and initialize a variable for points
+    let mut points = 100;
 
     loop {
-        let (min, max) = get_range();
+        println!("Please input your guess.");
 
-        let secret_number = rand::thread_rng().gen_range(min..=max);
-        
-        let mut guesses = 0;
+        let mut guess = String::new();
 
-        println!("Please enter your name:");
-        let mut name = String::new();
         io::stdin()
-            .read_line(&mut name)
-            .expect("Failed to read line");
-        let name = name.trim();
-
-        loop {
-            println!("Please enter your guess:");
-
-            let mut guess = String::new();
-
-            io::stdin()
-                .read_line(&mut guess)
-                .expect("Failed to read line");
-
-            let guess: u32 = match guess.trim().parse() {
-                Ok(num) => num,
-                Err(_) => {
-                    println!("Invalid input. Please enter a number.");
-                    continue;
-                }
-            };
-
-            guesses += 1;
-
-            println!("You guessed: {}", guess);
-
-            match guess.cmp(&secret_number) {
-                Ordering::Less => println!("Too small!"),
-                Ordering::Greater => println!("Too big!"),
-                Ordering::Equal => {
-                    println!("You win, {}! You took {} guesses.", name, guesses);
-                    break;
-                }
-            }
-        }
-
-        println!("Do you want to play again? (y/n)");
-
-        let mut play_again = String::new();
-        io::stdin()
-            .read_line(&mut play_again)
+            .read_line(&mut guess)
             .expect("Failed to read line");
 
-        if play_again.trim().to_lowercase() != "y" {
-            break;
-        }
-    }
-}
-
-fn get_range() -> (u32, u32) {
-    loop {
-        println!("Please enter the minimum value of the range:");
-        let mut min = String::new();
-        io::stdin()
-            .read_line(&mut min)
-            .expect("Failed to read line");
-        let min: u32 = match min.trim().parse() {
+        let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
-            Err(_) => {
-                println!("Invalid input. Using default value of 1.");
-                continue;
-            }
+            Err(_) => continue,
         };
 
-        println!("Please enter the maximum value of the range:");
-        let mut max = String::new();
-        io::stdin()
-            .read_line(&mut max)
-            .expect("Failed to read line");
-        let max: u32 = match max.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("Invalid input. Using default value of 100.");
-                continue;
+        // Increment guesses by one
+        guesses += 1;
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => {
+                println!("Too small!");
+                points -= 5;
             }
-        };
+            Ordering::Greater => {
+                println!("Too big!");
+                points -= 5;
+            }
+            Ordering::Equal => {
+                // Print guesses and name
+                let score = points - (guesses - 1) * 5;
+                println!("You win, {}! You took {} guesses and earned {} points.", name, guesses, score);
 
-        if min > max {
-            println!("Invalid range. Please enter a valid range.");
-            continue;
+                // Add score to the scoreboard
+                let mut scoreboard: HashMap<String, i32> = HashMap::new();
+                scoreboard.insert(name.to_string(), score);
+
+                // Print scoreboard
+                println!("Scoreboard:");
+                for (name, score) in &scoreboard {
+                    println!("{}: {}", name, score);
+                }
+
+                break;
+            }
         }
-
-        return (min, max);
     }
 }
