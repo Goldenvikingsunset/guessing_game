7f55b4233d976151fb7cd0fc1e18da7f3eea9fac
GoldenVikingSunset <Renni@GoldenVikingSunset.onmicrosoft.com>
2023-02-22 01:03:46 +0000

    refactored

--- src/main.rs
+++ src/main.rs
@@ -3,75 +3,99 @@ use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
-    println!("Guess the number!");
-
-    // Ask for the minimum and maximum values of the range
-    println!("Please enter the minimum value of the range:");
-    let mut min = String::new();
-    io::stdin()
-        .read_line(&mut min)
-        .expect("Failed to read line");
-    let min: u32 = match min.trim().parse() {
-        Ok(num) => num,
-        Err(_) => {
-            println!("Invalid input. Using default value of 1.");
-            1
-        }
-    };
-
-    println!("Please enter the maximum value of the range:");
-    let mut max = String::new();
-    io::stdin()
-        .read_line(&mut max)
-        .expect("Failed to read line");
-    let max: u32 = match max.trim().parse() {
-        Ok(num) => num,
-        Err(_) => {
-            println!("Invalid input. Using default value of 100.");
-            100
-        }
-    };
-
-    let secret_number = rand::thread_rng().gen_range(min..=max);
-    
-    // Declare and initialize a variable for guesses
-    let mut guesses = 0;
-
-    // Ask for and store the user name
-    println!("Please enter your name.");
-    let mut name = String::new();
-    io::stdin()
-        .read_line(&mut name)
-        .expect("Failed to read line");
-    let name = name.trim();
+    println!("Welcome to the guessing game!");
 
     loop {
-        println!("Please input your guess.");
+        let (min, max) = get_range();
 
-        let mut guess = String::new();
+        let secret_number = rand::thread_rng().gen_range(min..=max);
+        
+        let mut guesses = 0;
 
+        println!("Please enter your name:");
+        let mut name = String::new();
         io::stdin()
-            .read_line(&mut guess)
+            .read_line(&mut name)
             .expect("Failed to read line");
+        let name = name.trim();
 
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
+        loop {
+            println!("Please enter your guess:");
 
-        // Increment guesses by one
-        guesses += 1;
+            let mut guess = String::new();
 
-        println!("You guessed: {}", guess);
+            io::stdin()
+                .read_line(&mut guess)
+                .expect("Failed to read line");
 
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                // Print guesses and name
-                println!("You win, {}! You took {} guesses.", name, guesses);
-                break;
+            let guess: u32 = match guess.trim().parse() {
+                Ok(num) => num,
+                Err(_) => {
+                    println!("Invalid input. Please enter a number.");
+                    continue;
+                }
+            };
+
+            guesses += 1;
+
+            println!("You guessed: {}", guess);
+
+            match guess.cmp(&secret_number) {
+                Ordering::Less => println!("Too small!"),
+                Ordering::Greater => println!("Too big!"),
+                Ordering::Equal => {
+                    println!("You win, {}! You took {} guesses.", name, guesses);
+                    break;
+                }
             }
         }
+
+        println!("Do you want to play again? (y/n)");
+
+        let mut play_again = String::new();
+        io::stdin()
+            .read_line(&mut play_again)
+            .expect("Failed to read line");
+
+        if play_again.trim().to_lowercase() != "y" {
+            break;
+        }
+    }
+}
+
+fn get_range() -> (u32, u32) {
+    loop {
+        println!("Please enter the minimum value of the range:");
+        let mut min = String::new();
+        io::stdin()
+            .read_line(&mut min)
+            .expect("Failed to read line");
+        let min: u32 = match min.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Invalid input. Using default value of 1.");
+                continue;
+            }
+        };
+
+        println!("Please enter the maximum value of the range:");
+        let mut max = String::new();
+        io::stdin()
+            .read_line(&mut max)
+            .expect("Failed to read line");
+        let max: u32 = match max.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Invalid input. Using default value of 100.");
+                continue;
+            }
+        };
+
+        if min > max {
+            println!("Invalid range. Please enter a valid range.");
+            continue;
+        }
+
+        return (min, max);
     }
 }
